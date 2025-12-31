9dcc777db1e4284285c0b2bf361240601b8965d1
linuxiarznaetacie <148643643+linuxiarznaetacie@users.noreply.github.com>
2025-12-31 19:42:43 +0100

    Add files via upload

---
+++ nebula.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+HOST_MAN_LINK="" # Tutaj wklej swój link
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Ten skrypt wymaga uprawnień roota. Próba restartu z sudo..."
+  exec sudo "$0" "$@"
+fi
+
+apt install git -y
+cd /
+git clone https://github.com/linuxiarznaetacie/nebula-theme.git
+cd nebula-theme
+cp nebula.blueprint /var/www/pterodactyl/nebula.blueprint
+cd /var/www/pterodactyl
+blueprint -install nebula
+
+clear
+echo "+------------------------------------------------+"
+echo "        POMYSLNIE ZAINSTALOWANO MOTYW"
+echo "+------------------------------------------------+"
+
+read -p "Czy chcesz uruchomic menu glowne? (t/n): " odpowiedz
+
+if [[ "$odpowiedz" == "t" || "$odpowiedz" == "T" ]]; then
+    clear
+    if [ -n "$HOST_MAN_LINK" ]; then
+        bash <(curl -sSf "$HOST_MAN_LINK")
+    fi
+else
+    clear
+    exit 0
+fi
+
